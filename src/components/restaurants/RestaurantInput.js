b41b083dafada2dd85da22373492a99100282068
Liz Wilson <lizriderwilson@gmail.com>
2021-09-15 23:14:22 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor(props) {
+    super()
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            name="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)}
+          />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 
-class Review extends Component {
+class Review extends Component {handleOnClick = () => {
+    this.props.deleteReview(this.props.review.id)
+  }
 
   render() {
-    const { review } = this.props;
-
     return (
       <div>
         <li>
-          {review.text}
+          {this.props.review.text}
         </li>
-        <button> X </button>
+        <button onClick={this.handleOnClick}> x </button>
       </div>
     );
   }
-
 };
 
 export default Review;
